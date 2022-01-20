393dfa5bd48e043e18bb0f4a250070c2f419ea65
mert-cagil <74989130+mert-cagil@users.noreply.github.com>
2022-01-20 18:02:55 +0300

    evet

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+       
+
+
+            Console.ReadKey();
+        }
+    }
+}
